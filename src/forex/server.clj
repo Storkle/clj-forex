e21d1883fc2d06e86c3be186c99a1706d4380e2e
Storkle <user@email>
2010-09-12 15:19:29 -0500

    added project building system using leinigen. much easier to install now!

--- src/forex/server.clj
+++ src/forex/server.clj
@@ -12,8 +12,8 @@
 ;;that is the goal ....
 
 
-(ns server
-  (:use clojure.contrib.except clojure.contrib.def)
+(ns forex.server
+  (:use clojure.contrib.except clojure.contrib.def forex.tar.test)
   (:import
    (java.net Socket)
    (java.io PrintWriter InputStreamReader BufferedReader)))
@@ -134,29 +134,4 @@
   ([] (low 0))
   ([index] (mva 1 index :price +LOW+)))
 
-;;example usage
-(defn c?
-  ([direction tp] (c? direction tp 0))
-  ([direction tp index]
-     (let [h (+ (high index) (points 5))
-	   l (- (low index) (points 5))
-	   [price sl] (cond (= direction :up) (list h l)
-			    (= direction :down) (list l h)
-			    true (throwf "give me up or down!"))
-	   risk (Math/abs (- price sl))
-	   reward (Math/abs (- price tp))]
-       (print (format "PRICE %s%nTP %s%nSL %s%nRISK %s%nREWARD %s%nR/R %s" price tp sl (pips risk) (pips reward) (/ reward risk))))))
-
-;;usage
-(comment
-  ;;basically, you can either globally set the environment like this
-  (env! {:symbol "USDJPY" :timeframe +D1+})
-  (print (str "low back 1 is " (low 1)))
-					;or you can set the environment in a 'scope'
-  (wenv (:symbol "USDJPY" :timeframe +D1+) (print (str "low back 1 is " (low 1))))
-  ;;we first have to connect to the server by calling
-  (connect)
-
-  ;;and we will add more later! the metatrarder server actually has some more protocols programmed into it ....
-  )
 
---
+++ test/forex/test/core.clj
@@ -0,0 +1,7 @@
+(ns clj-forex.test.core
+ ; (:use [clj-forex.core] :reload)
+  (:use [clojure.test])
+)
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
