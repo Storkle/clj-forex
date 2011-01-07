5cfb79aaef51ee928a5bfde6f385378472c0a392
storkle <seth@Laurel.(none)>
2011-01-07 16:33:59 -0600

    move clojure code to literate programming .org file

--- project.clj
+++ project.clj
@@ -4,12 +4,14 @@
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [clj-time "0.2.0-SNAPSHOT"]		
                  [mmemail "1.0.1"]
+		 [clojureql "1.0.0-beta2-SNAPSHOT"]
+		 
                  ;;clj-forex 
                  [org.clojars.starry/clj-forex-utils "0.2"]
 		 [org.clojars.starry/clj-forex-indicators "0.1"]]   
    :dev-dependencies [[native-deps "1.0.5"]
 		      [swank-clojure "1.3.0-SNAPSHOT"]]
-   :native-dependencies [[org.clojars.starry/jzmq-native-deps "2.0.9.1"]]
+   :native-dependencies [[org.clojars.starry/jzmq-native-deps "2.0.10"]]
    :jvm-opts      ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8030"]
   ;;:main forex.binding
   ;;:aot :all
